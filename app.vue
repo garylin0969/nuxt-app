2878a57d1e7ff9c80e50407b8f1b8f64f4fc512d
Gary Lin <garylin0969@gmail.com>
2025-07-11 15:05:00 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+</script>
+
+<template>
+    <div class="bg-red-500">
+        <h1 class="text-3xl font-bold text-primary">Hello World</h1>
+        <Button class="bg-blue-500 text-white">Click me</Button>
+    </div>
+</template>
